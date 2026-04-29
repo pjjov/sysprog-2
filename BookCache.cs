0aba17773425884ef4ff4ebbea4c2ae471868b28
ClearBlueNight <nikola.vujicic2004@gmail.com>
2026-04-29 14:44:21 +0200

    Implementacija Cash'a

---
+++ BookCache.cs
@@ -0,0 +1,69 @@
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace SysProg;
+
+class BookCache
+{
+    private ReaderWriterLockSlim rwLock;
+    private Dictionary<string, JObject> responses;
+    private int max;
+    public BookCache(int max)
+    {
+        this.max = max;
+        this.responses = new Dictionary<string, JObject>();
+        this.rwLock = new ReaderWriterLockSlim();
+    }
+
+    public JObject? Find(string query)
+    {
+        JObject? result = null;
+        rwLock.EnterReadLock();
+
+        try
+        {
+            result = responses[query];
+        }
+        finally
+        {
+            rwLock.ExitReadLock();
+        }
+
+        return result;
+    }
+
+    public void Insert(string query, JObject result)
+    {
+        rwLock.EnterUpgradeableReadLock();
+        try
+        {
+            if (!responses.ContainsKey(query))
+            {
+                rwLock.EnterWriteLock();
+                try
+                { 
+                    if (responses.Count == max)
+                        responses.Remove(responses.First().Key);
+                    responses.Add(query, result);
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.ToString());
+                    throw;
+                }
+                finally
+                {
+                    rwLock.ExitWriteLock();
+                }
+                if (responses.Count == max)
+                {
+                    responses.Remove(responses.First().Key);
+                }
+            }
+        }
+        finally
+        {
+            rwLock.ExitUpgradeableReadLock();
+        }
+    }
+}
