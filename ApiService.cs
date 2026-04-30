6e32c6bc5a966cb5a4b98fd69b91f16bed0b40f9
Предраг Јовановић <predrag.jovanovic@elfak.rs>
2026-04-30 13:20:23 +0200

    Razdvoji URL funkcije u pomocnu klasu

--- ApiService.cs
+++ ApiService.cs
@@ -1,7 +1,7 @@
 using System.Net;
-using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using SysProg.Utils;
 
 namespace SysProg;
 
@@ -17,57 +17,16 @@ class ApiService {
         this.cache = new BookCache(cacheSize);
     }
 
-    private string buildUrl(string baseUrl, string url)
-    {
-        // return baseUrl + url + "&key=" + apiKey;
-        // ako ima ? u sredinu dodaje &, ali ako nema ? onda dodaje ?, ako ima na kraju onda ne dodaje nista, dodaje se na kraju pre kljuca
-        string separator = url.Contains("?") ? "&" : "?";
-        url += $"{separator}key={apiKey}";
-        return baseUrl + url;
-    }
-
     public JObject Fetch(string url)
     {   
-        HttpResponseMessage response = client.GetAsync(buildUrl(baseUrl, url)).Result;
+        HttpResponseMessage response = client.GetAsync(UrlUtils.BuildUrl(baseUrl, url, apiKey)).Result;
         response.EnsureSuccessStatusCode();
         string responseBody = response.Content.ReadAsStringAsync().Result;
         return JObject.Parse(responseBody);
     }
-    
-    private string NormalizeUrl(HttpListenerRequest request)
-    {
-        var query = request.QueryString;
-
-        var sortedKeys = query.AllKeys
-            .Where(k => k != null)
-            .OrderBy(k => k, StringComparer.Ordinal);
-
-        var sb = new StringBuilder();
-
-        foreach (var key in sortedKeys)
-        {
-            var values = query.GetValues(key);
-
-            foreach (var value in values)
-            {
-                if (sb.Length > 0)
-                    sb.Append("&");
-
-                sb.Append(Uri.EscapeDataString(key));
-                sb.Append("=");
-                sb.Append(Uri.EscapeDataString(value));
-            }
-        }
-
-        var baseUrl = request.Url.GetLeftPart(UriPartial.Path);
-
-        return sb.Length > 0
-            ? $"{baseUrl}?{sb}"
-            : baseUrl;
-    }
 
     public JObject Query(HttpListenerRequest req) {
-        string url = NormalizeUrl(req);
+        string url = UrlUtils.NormalizeUrl(req);
         JObject? result = cache.Find(url);
         if (result != null)
             return result;
---
+++ Utils/UrlUtils.cs
@@ -0,0 +1,47 @@
+using System.Net;
+using System.Text;
+
+namespace SysProg.Utils;
+
+public class UrlUtils {
+    public static string BuildUrl(string baseUrl, string path, string key)
+    {
+        // return base + path + "&key=" + key;
+        // ako ima ? u sredinu dodaje &, ali ako nema ? onda dodaje ?
+        string separator = path.Contains("?") ? "&" : "?";
+        path += $"{separator}key={key}";
+        return baseUrl + path;
+    }
+
+    public static string NormalizeUrl(HttpListenerRequest request)
+    {
+        var query = request.QueryString;
+
+        var sortedKeys = query.AllKeys
+            .Where(k => k != null)
+            .OrderBy(k => k, StringComparer.Ordinal);
+
+        var sb = new StringBuilder();
+
+        foreach (var key in sortedKeys)
+        {
+            var values = query.GetValues(key);
+
+            foreach (var value in values)
+            {
+                if (sb.Length > 0)
+                    sb.Append("&");
+
+                sb.Append(Uri.EscapeDataString(key));
+                sb.Append("=");
+                sb.Append(Uri.EscapeDataString(value));
+            }
+        }
+
+        var baseUrl = request.Url.GetLeftPart(UriPartial.Path);
+
+        return sb.Length > 0
+            ? $"{baseUrl}?{sb}"
+            : baseUrl;
+    }
+}
